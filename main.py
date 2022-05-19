d6561b0c749937a8267d72eac3b50e82498fa28b
mike-mp3 <73157813+mike-mp3@users.noreply.github.com>
2022-05-20 02:11:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+
+print("Working...")
+
+
+def main():
+    chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    number = (random.randint(8, 12))
+    length = (random.randint(8, 12))
+    for n in range(number):
+        password = ''
+        for i in range(length):
+            password += random.choice(chars)
+        return password
+
+
+d = open('data.txt', 'a+')
+print("*" * 10 + " <Password manager> " + "*" * 10)
+print("Write down how many passwords you need")
+c = int(input())
+while c != 0:
+    d.write(f'{main()} + \n')
+    c -= 1
+print('Done! Saves go to data.txt')
+d.close()
